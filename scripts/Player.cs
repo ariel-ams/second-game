25ef9c663d8d194e01c87eb84520f968bd90634b
Ariel Medina <5546182+ariel-ams@users.noreply.github.com>
2024-08-03 16:28:49 +1200

    refactor: Adjust player speed and jump velocity constants

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -400.0f;
+	public const float Speed = 125.0f;
+	public const float JumpVelocity = -308.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
